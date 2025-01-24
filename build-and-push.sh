4db6db7abbff91bfcfaacf36dc8a9b9feada15b2
Nick Savage <nick@nicksavage.ca>
2025-01-24 16:35:01 -0500

    typo

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,7 +7,7 @@ set +a
 
 # Build and push frontend
 echo "Building frontend..."
-docker build -t --build-arg VITE_API_URL=https://codescribble.com nsavage/codescribble-frontend:latest -f ./frontend/Dockerfile ./frontend
+docker build --build-arg VITE_API_URL=https://codescribble.com -t nsavage/codescribble-frontend:latest -f ./frontend/Dockerfile ./frontend
 docker push nsavage/codescribble-frontend:latest
 
 # Build and push backend
